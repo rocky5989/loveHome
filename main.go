6e493b4b90d0e5e7437003c2414e85b86aee97b1
rocky5989 <rocky5989@163.com>
2018-03-03 18:57:58 +0800

    modify filter by rocky

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	_ "loveHome/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "loveHome/routers"
+	"net/http"
+	"strings"
 )
 
 func main() {
+
+	ignoreStatic()
 	beego.Run()
 }
 
+func ignoreStatic() {
+
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
+
+}
+
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+
+	beego.Debug("requst url:", orpath)
+
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+
+}
